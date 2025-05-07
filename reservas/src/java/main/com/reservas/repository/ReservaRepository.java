d8a7da7b0a63d222cbf7a43e96052ba87480e1df
YiT <yitzhaknamihas@gmail.com>
2025-05-07 15:11:56 -0500

    Delete reservas directory

--- reservas/src/java/main/com/reservas/repository/ReservaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.tuempresa.reservas.repository;
-
-import com.tuempresa.reservas.model.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
