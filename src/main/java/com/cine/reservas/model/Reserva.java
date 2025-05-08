d8c3656a373615f9dc0b896cd509e906dbac69f3
Yit 73 <yitzhaknamihas@gmail.com>
2025-05-08 08:00:02 -0500

    cliente -> usuario

--- src/main/java/com/cine/reservas/model/Reserva.java
+++ src/main/java/com/cine/reservas/model/Reserva.java
@@ -22,7 +22,7 @@ public class Reserva {
 
     @NotNull
     @Size(min = 3, max = 100, message = "El nombre del cliente debe tener entre 3 y 100 caracteres")
-    private String cliente;
+    private String usuario;
 
     @NotNull
     private LocalDate fecha;
