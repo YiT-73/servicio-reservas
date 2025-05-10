29900f6bcb763323241b9e5480d442509132d060
Yit 73 <yitzhaknamihas@gmail.com>
2025-05-09 23:56:00 -0500

    intento de implementacion de db

--- src/main/java/com/cine/reservas/ServicioReservasApplication.java
+++ src/main/java/com/cine/reservas/ServicioReservasApplication.java
@@ -2,7 +2,9 @@ package com.cine.reservas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+@EnableMongoRepositories(basePackages = "com.cine.reservas.repository")
 @SpringBootApplication
 public class ServicioReservasApplication {
 
--- src/main/java/com/cine/reservas/model/Reserva.java
+++ src/main/java/com/cine/reservas/model/Reserva.java
@@ -1,24 +1,21 @@
 package com.cine.reservas.model;
 
+import jakarta.validation.constraints.NotNull;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-import org.antlr.v4.runtime.misc.NotNull;
 
 import java.time.LocalDate;
+
 @Getter
 @Setter
-@Entity
 @Document(collection = "reservas")
 public class Reserva {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private String id; // MongoDB usa id tipo String por defecto
 
     @NotNull
     @Size(min = 3, max = 100, message = "El nombre del cliente debe tener entre 3 y 100 caracteres")
@@ -28,12 +25,11 @@ public class Reserva {
     private LocalDate fecha;
 
     @NotNull
-    private  String pelicula_nombre;
+    private String pelicula_nombre;
 
     @NotNull
     private Long peliculaId;
 
     @NotNull
     private Long usuarioId;
-
 }
